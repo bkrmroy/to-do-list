b61ad31e306b9d2fec1bb10bcf45d74661094bfe
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-09-06 21:34:17 +0600

    Add button for adding new task
    New form pops up when user click add task button.
    will change th position of the button later

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const projectFormSubmit = document.querySelector('.btn-project-form-submit')
 const projectFormOverlay = document.querySelector('.project-form-overlay')
 const projectSection = document.querySelector('.project-section')
 const content =document.querySelector('.content')
+const btnAddTask = document.querySelector('.btn-new-task')
+const taskFormContainer = document.querySelector('.task-form-container')
+const taskFormOverlay = document.querySelector('.task-form-overlay')
+const taskForm =document.querySelector('.task-form')
+const taskFormSubmit = document.querySelector('.btn-task-form-submit')
+
 const handleProjectForm = (()=>{
     btnAddProject.addEventListener('click',()=>{
         openProjectForm()
@@ -51,7 +57,33 @@ function removeClass(){
         content.classList.remove(itemOfClassList)
     })
 }
-
+const openTaskForm = ()=>{
+    taskFormContainer.classList.add('active')
+    taskFormOverlay.classList.add('active')
+}
+const closeTaskForm = ()=>{
+    taskFormContainer.classList.remove('active')
+    taskFormOverlay.classList.remove('active')
+    taskForm.reset()
+}
+const handleTaskForm = (()=>{
+    btnAddTask.addEventListener('click',()=>{
+        openTaskForm()
+    })
+    taskFormOverlay.addEventListener('click',function(){
+        closeTaskForm()
+    })
+    taskFormSubmit.addEventListener('click',(event)=>{
+        event.preventDefault()
+        if(!taskForm.checkValidity()){
+            taskForm.reportValidity()
+        }
+        if(taskForm.checkValidity()){
+            ////////////////////
+            closeTaskForm()
+        }
+    })
+})()
 //when user clicks taqke th name of the clicked item and pas s in as a parameter
 //take that parameter and bring out the array from the projectLibray function
 // loop the array and show every item
